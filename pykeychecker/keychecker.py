70dd5728478e6e7c66cf4652f2e8636bb74e7390
Arthur Breitman <Arthur Breitman>
2017-07-14 04:08:15 -0700

    restore private key check functionality

--- pykeychecker/keychecker.py
+++ pykeychecker/keychecker.py
@@ -17,7 +17,8 @@ def bitcoin_address(digest):
         [digest, 117,  2] + bitcoin_keys + [2, 174])
     return bitcoin.p2sh_scriptaddr(script)
 
-def ethereum_data(digest, hexdigest):
+def ethereum_data(digest):
+    hexdigest = binascii.hexlify(digest)
     checksum = sha256(sha256(digest).digest()).hexdigest()[:8]
     return "0x946941ec" + hexdigest + checksum
 
@@ -28,12 +29,40 @@ def ethdata_to_tz1(ethdata):
     return ethdata[10:-8]
 
 if __name__ == '__main__':
-    tz_input = sys.argv[1]
-    assert(tz_input == bitcoin.bin_to_b58check(bitcoin.b58check_to_bin(tz_input)[2:], magicbyte=434591))
-    
-    digest = bitcoin.b58check_to_bin(tz_input)[2:]
-    hexdigest = binascii.hexlify(digest)
+        
+    if len(sys.argv) == 18:
+        
+        mnemonic = ' '.join(sys.argv[1:16]).lower()
+        email = sys.argv[16]
+        password = sys.argv[17]
+        salt = unicodedata.normalize(
+            "NFKD", (email + password).decode("utf8")).encode("utf8")
+        try:
+            seed = bitcoin.mnemonic_to_seed(mnemonic, salt)
+        except:
+            print("Invalid mnemonic")
+            exit(1)
+        pk, sk = pysodium.crypto_sign_seed_keypair(seed[0:32])
+        pkh = blake2b(pk,20).digest()
 
-    print "public key hash: ", tezos_pkh(digest)
-    print "Ethereum data:   ", ethereum_data(digest, hexdigest)
-    print "bitcoin address: ", bitcoin_address(digest)
+        print "public key hash: ", tezos_pkh(pkh)
+    
+    elif  len(sys.argv) == 2:
+        tz_input = sys.argv[1]
+        assert(tz_input == bitcoin.bin_to_b58check(bitcoin.b58check_to_bin(tz_input)[2:], magicbyte=434591))
+
+        try:
+            pkh = bitcoin.b58check_to_bin(tz_input)[2:]
+        except:
+            print "Invalid public key hash"
+
+    else:
+        print("""Usage:
+python keychecker.py garage absurd steak ...  email password
+or
+python keychecker.py tz1YoUrPuBlicKeYhaSh""")
+        exit(1)
+
+            
+    print "Ethereum data:   ", ethereum_data(pkh)
+    print "Bitcoin address: ", bitcoin_address(pkh)
